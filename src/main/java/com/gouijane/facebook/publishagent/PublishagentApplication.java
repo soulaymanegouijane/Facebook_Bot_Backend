bed56eacf3b791ccaeb81c2bf536e572e56d8555
soulaymanegouijane <soulaymanegouijane@gmail.com>
2024-08-24 15:43:02 +0200

    project

---
+++ src/main/java/com/gouijane/facebook/publishagent/PublishagentApplication.java
@@ -0,0 +1,13 @@
+package com.gouijane.facebook.publishagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublishagentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublishagentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gouijane/facebook/publishagent/PublishagentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gouijane.facebook.publishagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublishagentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
